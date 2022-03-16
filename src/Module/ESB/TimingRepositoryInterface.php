4b4ebc91a762e64d3274082e529b21de50dc4deb
vikbert <segentor@gmail.com>
2022-03-17 00:27:31 +0100

    update contract demo

--- src/Module/ESB/TimingRepositoryInterface.php
+++ src/Module/ESB/TimingRepositoryInterface.php
@@ -7,6 +7,8 @@ namespace App\Module\ESB;
 interface TimingRepositoryInterface
 {
     public function openingTimes(string $countryCode): iterable;
+
     public function specialOpeningTimes(string $countryCode): iterable;
+
     public function holidayOpeningTimes(string $countryCode): iterable;
 }
